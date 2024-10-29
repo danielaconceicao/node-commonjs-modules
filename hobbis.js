ece301762cdb89ae28ef327f17b044002ab17012
danielaconceicao <daniela.3597@gmail.com>
2024-10-29 14:58:08 +0100

    first and second step completed

---
+++ hobbis.js
@@ -0,0 +1,7 @@
+function hobby(hobbyOne, hobbyTwo, hobbyThree){
+    const robbies = [];
+    robbies.push(hobbyOne, hobbyTwo, hobbyThree);
+    return robbies
+}
+
+console.log(hobby('futebol', 'serie', 'cachaca'));
---
+++ names.js
@@ -0,0 +1,14 @@
+function names(firstName, lastName){
+    const fullName = [
+        {
+            name: `${firstName}`,
+            surName: `${lastName}` 
+        }
+    ];
+    return fullName
+}
+
+console.log(names('Daniela', 'Conceicao'));
+
+
+
